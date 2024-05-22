52e28900fe8e2b1dc13315b3188566c1cc963435
Luzifer197 <fasanellamassimo16@gmail.com>
2024-05-22 09:15:57 +0200

    move deploy.js

--- src/commands/deploy/deploy.js
+++ src/commands/deploy/deploy.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { exec } = require('child_process');
-const Logger = require('../function/logger');
+const Logger = require('../../function/logger');
 
 module.exports = {
     data: new SlashCommandBuilder()
